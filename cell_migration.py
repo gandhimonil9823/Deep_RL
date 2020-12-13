243db861ae00f9b76420f1cb1ce27d818af40e03
gandhimonil9823 <31587347+gandhimonil9823@users.noreply.github.com>
2020-12-13 03:14:37 -0800

    Delete cell_migration.py

--- cell_migration.py
+++
@@ -1,7 +0,0 @@
-# @article{wang2018deep,
-#   title={Deep Reinforcement Learning of Cell Movement in the Early Stage of C. elegans Embryogenesis},
-#   author={Wang, Zi and Wang, Dali and Li, Chengcheng and Xu, Yichi and Li, Husheng and Bao, Zhirong},
-#   journal={arXiv preprint arXiv:1801.04600},
-#   year={2018}
-# }
-
